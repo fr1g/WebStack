7d9971a2e889d758e13ab6de8c266e0313e9a633
Feiron <53085628+fr1g@users.noreply.github.com>
2024-07-11 03:53:37 +0800

    continue: backyard of submissions management

--- construct/api/api.php
+++ construct/api/api.php
@@ -7,29 +7,84 @@ $extra_accessedIPs = isset($tmp) ? json_decode($tmp, true) : [];
 $tmp = null;
 $tmp = $redis -> get('isDbSet');
 $dbCreated = $tmp ?? isset($tmp);
+$_debugDataSet = [];
+$_debugDataSet['oxx'] = 'xx';
 try {
-    if(!$dbCreated){
-        // if in redis, no record that the database table already created
+    $_debugDataSet['dbCreated'] = var_export($dbCreated, true);
+    if(!(var_export($dbCreated, true) == "'true'")){ // redis: no table then check if table do lost
+        $_debugDataSet['beginNoDb'] = 'pinned';
+        $__actuallyExist = false;
+        $entireListDb = $GLOBALS['wpdb'] -> get_results("show tables", 'ARRAY_A');
+        foreach($entireListDb as $k => $v){
+            if($v == 'wp_ex_submissions') {
+                $__actuallyExist = true;
+                break;
+            }
+        }
+        if(!$__actuallyExist){
+            $___table = "create table wp_ex_submissions(
+                id int auto_increment unique not null, 
+                name varchar(64) not null, 
+                link varchar(128) not null,
+                easy varchar(128) not null,
+                type varchar(64) not null,
+                stat varchar(10) not null default 'pending',
+                dscr text
+            )";
+            $GLOBALS['wpdb'] -> query($___table);
+        }
+        // let redis know actually exists
+        writeRedis('isDbSet', 'true', true);
+        $GLOBALS['dbCreated'] = 'true';
+        $_debugDataSet['tables'] = $GLOBALS['wpdb'] -> get_results("show tables", 'ARRAY_A');
+        $_debugDataSet['isdbc'] = readRedis('isDbSet', true);
 
-        // if in mysql exists the table
-
-        // else, create the table
+    }
+    else{ // redis认为有表，实际上mysql没表，也会进入这里。
+        $_debugDataSet['db'] = 'already exists# if not, check if the necessary table "wp_ex_submissions" exists.';
     }
 } catch (\Throwable $th) {
     //throw $th;
+    dbbp(`$th exception`);
 }
 
 $redis -> close();
 
 $MAX_TRY = 2;
-$COOL_DOWN = 300; // seconds
+$COOL_DOWN = 30;//0; // seconds
 // to debug it in small time span, modify it to 30.
 
-function submitSync(){
-    $GLOBALS["redis"] -> connect('localhost', 6379);
-    $GLOBALS["redis"] -> set('ips', json_encode($GLOBALS["extra_accessedIPs"]));
-    $GLOBALS["extra_accessedIPs"] = json_decode(($GLOBALS["redis"]) -> get('ips'), true);
+function dbbp ($msg){
+    $GLOBALS['_debugDataSet'][`PointMsg: $msg`] = 'pinned';
+}
+
+function writeRedis($key, $text, $dontClose = false){
+    openRedis();
+    $GLOBALS["redis"] -> set($key, $text);
+    if($dontClose) return;
+    closeRedis();
+}
+
+function readRedis($key, $dontClose = false){
+    openRedis();
+    $rtemp_ = $GLOBALS["redis"] -> get($key);
+    if($dontClose) return $rtemp_;
+    closeRedis();
+    return $rtemp_;
+}
+
+function closeRedis(){
     $GLOBALS["redis"] -> close();
+}
+
+function openRedis($host = 'localhost', $port = 6379){
+    $GLOBALS["redis"] -> connect($host, $port);
+}
+
+function submitSync(){
+    writeRedis('ips', json_encode($GLOBALS["extra_accessedIPs"]), true);
+    $GLOBALS["extra_accessedIPs"] = json_decode(($GLOBALS["redis"]) -> get('ips'), true);
+    closeRedis();
 }
 
 function pushIpAccessedStatus($ipaddr){
@@ -68,13 +123,16 @@ add_action('rest_api_init', function() {
         'methods' => 'GET',
         'callback' => function (){
             $filtered = '';
-            $ress = $GLOBALS['wpdb'] -> get_results("select slug from wp_terms where slug like '%-type' and 1=1", 'ARRAY_A');
+            $ress = $GLOBALS['wpdb'] -> get_results("select slug, name from wp_terms where slug like '%-type' and 1=1", 'ARRAY_A');
             foreach ($ress as $k => $item) {
-                $filtered = $filtered . '###' . $item['slug'];
-            }
+                $filtered = $filtered . '###' . $item['name'] . ' ('. $item['slug'] . ')';
+            } // 如果能解决检查分类变更的话，可以考虑缓存在redis
             return [
                 'groups' => $filtered,
-                'dbc' => $GLOBALS['dbCreated']
+                'dbc' => $GLOBALS['_debugDataSet'],
+                'now' => time(),
+                'isex' => readRedis('isDbSet'),
+                'xx' => $GLOBALS['dbCreated']
             ];
         }
     ]);
@@ -90,12 +148,22 @@ add_action('rest_api_init', function() {
             if(!$isSuccessed) return [
                 'status' => false,
                 'instance' => ($inst ? $inst : 'unset_yet'),
+                'msg' => 'overflow'
             ];
 
             $respBody = file_get_contents('php://input');
             // 
-            $fill = json_decode($respBody);
-            
+            $fill = json_decode($respBody, true);
+            if(($fill['name'] ?? '') == '' || ($fill['link'] ?? '') == '' || ($fill['easy'] ?? '') == '' || ($fill['type'] ?? '') == '' || (strlen($fill['dscr']) / 3) >= 100){
+                return [
+                    'status' => false,
+                    'instance' => ($inst ? $inst : 'unset_yet'),
+                    'msg' => 'wrong data',
+                    'chk' => $fill
+                ];
+            }
+
+            $GLOBALS['wpdb'] -> insert('wp_ex_submissions', $fill);
             
             return [
                 'status' => $isSuccessed,
--- construct/functions.php
+++ construct/functions.php
@@ -4,7 +4,7 @@ include('api/api.php');
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 date_default_timezone_set('Asia/Shanghai');
 require get_template_directory() . '/inc/inc.php';
-
+require_once get_template_directory() . '/inc/frame/cs-framework.php';
    
 //登录页面的LOGO链接为首页链接
 add_filter('login_headerurl',function() {return get_bloginfo('url');});
@@ -15,4 +15,20 @@ add_filter('login_headertext',function() {return get_bloginfo( 'description' );}
 add_action( 'wp_enqueue_scripts', 'fanly_remove_block_library_css', 100 );
 function fanly_remove_block_library_css() {
 	wp_dequeue_style( 'wp-block-library' );
+}
+
+add_action('admin_menu', 'submissionProccPage');
+function submissionProccPage(){
+    add_menu_page( 
+        '管理投稿', 
+        '管理投稿:', 
+        'manage_options', 
+        's-man', 
+        'submissionProccPageRender', 
+        'dashicons-admin-customizer', 
+        6 
+    ); 
+}
+function submissionProccPageRender() {
+    include(get_template_directory() . '/api/manage.php');
 }
